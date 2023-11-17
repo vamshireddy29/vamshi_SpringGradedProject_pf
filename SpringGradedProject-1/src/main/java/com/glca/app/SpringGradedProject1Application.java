9b180e4062cd2d87c1e302e3527c572b9cfc034c
vamshireddy29 <140611393+vamshireddy29@users.noreply.github.com>
2023-11-17 22:56:56 +0530

    Add files via upload

---
+++ SpringGradedProject-1/src/main/java/com/glca/app/SpringGradedProject1Application.java
@@ -0,0 +1,13 @@
+package com.glca.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGradedProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGradedProject1Application.class, args);
+	}
+
+}
---
+++ SpringGradedProject-1/src/test/java/com/glca/app/TicketTrackerapplicationTests.java
@@ -0,0 +1,5 @@
+package com.glca.app;
+
+public class TicketTrackerapplicationTests {
+
+}
